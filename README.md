a6016c78f21fce7498da22b86deba7351d428382
Tarun Saxena <tarunsaxena@ttnpl-3740.local>
2023-03-10 13:48:59 +0530

    Updated app

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sample python monolith application
+
+## Tech Stack
+- Flask
+- Python 3.7.16
+
+## Build Instructions
+
+```
+git clone git@github.com:tarunsaxenattn/sample-python-monolith-app.git
+docker build . -t sample-python-monolith-app -f docker/Dockerfile
+```
+
+## Run App locally
+
+```
+docker run -itd -p 5000:5000 sample-python-monolith-app
+```
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/payment')
+def payment():
+    return 'Payment Service is healthy'
+
+@app.route('/login')
+def login():
+    return 'Login Service is healthy'
+
+@app.route('/ui')
+def ui():
+    return 'UI Service is healthy'
+
+@app.route('/health')
+def health():
+    return 'app is healthy'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
